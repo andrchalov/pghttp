337c538b4f2c23d131da3e68eec139cc73f6fcf8
Andrey Chalov <andrchalov@gmail.com>
2019-11-18 14:59:14 +0300

    Fix multipart_to_json helper issue

--- sql/functional/functions/helpers/multipart_to_json.sql
+++ sql/functional/functions/helpers/multipart_to_json.sql
@@ -11,11 +11,11 @@ import requests
 import cgi
 from requests_toolbelt.multipart.decoder import MultipartDecoder
 
-decoder = MultipartDecoder(body, content_type)
+decoder = MultipartDecoder(body.encode(), content_type)
 
 result = {}
 for part in decoder.parts:
-  value, params = cgi.parse_header(part.headers['Content-Disposition'])
+  value, params = cgi.parse_header(part.headers[b'Content-Disposition'].decode())
   result[params['name']] = part.text
 
 return json.dumps(result)
